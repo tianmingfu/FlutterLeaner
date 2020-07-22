1a4aa0c0d8157150ea41cf54e8b39e6a4b48a91f
tianmingfu <mingfu.tian@dataenlighten.com>
2020-07-22 17:38:34 +0800

    线性布局 textDirection：表示水平方向子组件的布局顺序(是从左往右还是从右往左) mainAxisSize：表示Row在主轴(水平)方向占用的空间mainAxisAlignment：表示子组件在Row所占用的水平空间内对齐方式 verticalDirection：表示Row纵轴（垂直）的对齐方向，默认是VerticalDirection.down，表示从上到下。 crossAxisAlignment：表示子组件在纵轴方向的对齐方式 children ：子组件数组。

--- lib/TipRoute.dart
+++ lib/TipRoute.dart
@@ -16,59 +16,121 @@ print("argsargsargsargs:"+args);
     return Scaffold(
       appBar: AppBar(
         title: Text("提示"),
+        backgroundColor: Colors.amberAccent,
       ),
-      body: Padding(
-        padding: EdgeInsets.all(19),
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              Text(text),
-              RaisedButton(
-                onPressed: ()=>Navigator.pop(context,"我是返回值"),
-                child: Text("返回"),
-              ),
-              LinearProgressIndicator(
-                backgroundColor: Colors.grey[200],
-                valueColor: AlwaysStoppedAnimation(Colors.blue),
-              ),
-//进度条显示50%
-              LinearProgressIndicator(
-                backgroundColor: Colors.grey[200],
-                valueColor: AlwaysStoppedAnimation(Colors.blue),
-                value: .5,
-              ),
-              CircularProgressIndicator(
-                backgroundColor: Colors.grey[200],
-                valueColor: AlwaysStoppedAnimation(Colors.red),
-              ),
-              CircularProgressIndicator(
-                backgroundColor: Colors.grey[200],
-                valueColor: AlwaysStoppedAnimation(
-                  Colors.blue
-                ),
-                value: .5,
-              ),
-              SizedBox(
-                height: 3,
-                child: LinearProgressIndicator(
-                  backgroundColor: Colors.grey[200],
-                  valueColor: AlwaysStoppedAnimation(Colors.blue),
-                  value: .7,
-                ),
-              ),
-              SizedBox(
-                height: 100,
-                width: 100,
-                child: CircularProgressIndicator(
-                  backgroundColor: Colors.grey[200],
-                  valueColor: AlwaysStoppedAnimation(Colors.red),
-                  value: .6,
-                ),
-              )
-            ],
-          ),
+      body:Container(
+        color: Colors.green,
+        child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Expanded(child: Container(
+                  color: Colors.red,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Hello world"),
+                      Text("I am Jack"),
+                    ],
+                  ),
+                ))
+              ],
+            ),
         ),
-      ),
+      )
+//      Padding(
+//        padding: EdgeInsets.all(19),
+//        child: Center(
+//          child: Column(
+//            crossAxisAlignment: CrossAxisAlignment.start,
+//            children: <Widget>[
+//              Text(text),
+//              RaisedButton(
+//                onPressed: ()=>Navigator.pop(context,"我是返回值"),
+//                child: Text("返回"),
+//              ),
+//              LinearProgressIndicator(
+//                backgroundColor: Colors.grey[200],
+//                valueColor: AlwaysStoppedAnimation(Colors.blue),
+//              ),
+////进度条显示50%
+//              LinearProgressIndicator(
+//                backgroundColor: Colors.grey[200],
+//                valueColor: AlwaysStoppedAnimation(Colors.blue),
+//                value: .5,
+//              ),
+//              CircularProgressIndicator(
+//                backgroundColor: Colors.grey[200],
+//                valueColor: AlwaysStoppedAnimation(Colors.red),
+//              ),
+//              CircularProgressIndicator(
+//                backgroundColor: Colors.grey[200],
+//                valueColor: AlwaysStoppedAnimation(
+//                  Colors.blue
+//                ),
+//                value: .5,
+//              ),
+//              SizedBox(
+//                height: 3,
+//                child: LinearProgressIndicator(
+//                  backgroundColor: Colors.grey[200],
+//                  valueColor: AlwaysStoppedAnimation(Colors.blue),
+//                  value: .7,
+//                ),
+//              ),
+//              SizedBox(
+//                height: 100,
+//                width: 100,
+//                child: CircularProgressIndicator(
+//                  backgroundColor: Colors.grey[200],
+//                  valueColor: AlwaysStoppedAnimation(Colors.red),
+//                  value: .6,
+//                ),
+//              ),
+//              Row(
+//                mainAxisAlignment: MainAxisAlignment.center,
+//                children: [
+//                  Text(" hello world "),
+//                  Text(" I am Jack "),
+//                ],
+//              ),
+//              Row(
+//                mainAxisAlignment: MainAxisAlignment.center,
+//                mainAxisSize: MainAxisSize.min,
+//                children: [
+//                  Text(" hello world "),
+//                  Text(" I am Jack "),
+//                ],
+//              ),
+//              Row(
+//                mainAxisAlignment: MainAxisAlignment.end,
+//                textDirection: TextDirection.rtl,
+//                children: <Widget>[
+//                  Text(" hello world "),
+//                  Text(" I am Jack "),
+//                ],
+//              ),
+//              Row(
+//                crossAxisAlignment: CrossAxisAlignment.start,
+//                verticalDirection: VerticalDirection.up,
+//                children: <Widget>[
+//                  Text(" hello world ", style: TextStyle(fontSize: 30.0),),
+//                  Text(" I am Jack "),
+//                ],
+//              ),
+//              Column(
+//                crossAxisAlignment: CrossAxisAlignment.center,
+//                children: [
+//                  Text("Hi"),
+//                  Text("world")
+//                ],
+//              )
+//            ],
+//          ),
+//        ),
+//      ),
 
 
 
